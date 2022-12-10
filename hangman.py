340e524b45fa3518c1be0ab770967bd10e8cddac
franco <117970385+francokiid@users.noreply.github.com>
2022-12-10 16:32:38 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -18,26 +18,17 @@ class HangmanGame(WordGenerator):
         print_sleep("Make 7 wrong guesses and you LOSE. The man will die!\n\n", 2)
         
     def login(self):
-        pass
         while True:
-
             try:
-
                 name = input("Enter player name: ")
-
                 if not name:
-
                     raise ValueError
-
                 self.player_name = Player(name)
-
                 self.play_game()
-
                 break
-
             except ValueError:
-
                 print("Oops! A name is required to play the game.\n")
+                
     def play_game(self):
         pass
         
