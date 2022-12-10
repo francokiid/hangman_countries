f9e9489e35aaa898849c2d1002a170a632b95261
j-balmes <117633129+j-balmes@users.noreply.github.com>
2022-12-10 16:31:36 +0800

    Create hangman.py

--- hangman.py
+++ hangman.py
@@ -19,9 +19,25 @@ class HangmanGame(WordGenerator):
         
     def login(self):
         pass
-        
+        while True:
+
+            try:
+
+                name = input("Enter player name: ")
+
+                if not name:
+
+                    raise ValueError
+
+                self.player_name = Player(name)
+
+                self.play_game()
+
+                break
+
+            except ValueError:
+
+                print("Oops! A name is required to play the game.\n")
     def play_game(self):
         pass
-        
-        
         
