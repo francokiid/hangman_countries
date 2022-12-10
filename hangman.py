32328302815089a5bf7a4e007397d9ceb4762c6d
franco <117970385+francokiid@users.noreply.github.com>
2022-12-10 16:36:56 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+from word_generator import WordGenerator
+
 class HangmanGame(WordGenerator):
     def __init__(self):
         self.player_name = ""
@@ -12,11 +14,8 @@ class HangmanGame(WordGenerator):
                 self.display.append("_")
                 
     def greeting(self):
-        input("\nLet's play a classic game of HANGMAN: COUNTRIES!\n\n\nPress the <ENTER> key to continue...")
-        print_sleep("\n\nThe goal is to save the man from being hung!", 2)
-        print_sleep("I will choose a random country, and you have to guess it one letter at a time.", 2)
-        print_sleep("Make 7 wrong guesses and you LOSE. The man will die!\n\n", 2)
-        
+        pass
+    
     def login(self):
         while True:
             try:
