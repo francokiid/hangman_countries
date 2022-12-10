75ece111902dc95e89e133cb9da44798c40efbe3
franco <117970385+francokiid@users.noreply.github.com>
2022-12-10 18:08:34 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,13 @@
+from getpass import getpass
+from os import system
+
 from word_generator import WordGenerator
+from player import Player
 from utils.print_sleep import PrintSleep
 
+
 print_sleep = PrintSleep().ps
+
 class HangmanGame(WordGenerator):
     def __init__(self):
         self.player_name = ""
@@ -16,7 +22,10 @@ class HangmanGame(WordGenerator):
                 self.display.append("_")
                 
     def greeting(self):
-        pass
+        getpass("\nLet's play a classic game of HANGMAN: COUNTRIES!\n\n\nPress the <ENTER> key to continue...")
+        print_sleep("\n\nThe goal is to save the man from being hung!", 2)
+        print_sleep("I will choose a random country, and you have to guess it one letter at a time.", 2)
+        print_sleep("Make 7 wrong guesses and you LOSE. The man will die!\n\n", 2)
     
     def login(self):
         while True:
@@ -29,7 +38,37 @@ class HangmanGame(WordGenerator):
                 break
             except ValueError:
                 print("Oops! A name is required to play the game.\n")
+
+    def update_display(self):
+        for idx, letter in enumerate(self.hidden_word):
+            if self.hidden_word[idx] in self.guesses:
+                self.display[idx] = letter
+
+    def take_guess(self):
+        pass
+    
+    def check_guess(self):
+        pass
+                
+    def check_win(self):
+        pass                
+                
+    def get_hint(self):
+        pass             
+
+    def play_again(self):
+        pass
+    
+    def logout(self):
+        pass  
+    
+    def goodbye(self):
+        pass         
                 
     def play_game(self):
-        pass
-        
+        self.greeting()
+        self.login()
+
+if __name__ == "__main__":
+    hangman = HangmanGame()
+    hangman.play_game()
