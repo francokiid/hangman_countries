bea6da1889e1c9bb881638693b85de0349f950f7
franco <117970385+francokiid@users.noreply.github.com>
2022-12-11 12:30:45 +0800

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -2,11 +2,14 @@ from random import choice
 
 
 class WordGenerator:
-    words = {}
-    with open("countries_and_capitals.txt", encoding = "utf-8") as f:
-        for line in f:
-            (country, capital) = line.split("/")
-            words[country.strip()] = capital.strip()
-    
-    def get_country(self):
-        return choice(list(self.words.keys()))
+    def __init__(self):
+        self.words = {}
+        try:
+            with open("countries_and_capitals.txt", encoding = "utf-8") as f:
+                for line in f:
+                    (country, capital) = line.split("/")
+                    words[country.strip()] = capital.strip()
+        except OSError:
+            print("Oops! Could not open/read file containing countries and capitals.")
+            print("Please make sure you have countries_and_capitals.txt in your folder.")
+        self.hidden_word = choice(list(self.words.keys()))
