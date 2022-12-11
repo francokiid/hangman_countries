e7d50ebd597687b1bff6e42f77235361940f9c9f
franco <117970385+francokiid@users.noreply.github.com>
2022-12-12 03:36:22 +0800

    Update word_generator.py

--- word_generator.py
+++ word_generator.py
@@ -1,14 +1,17 @@
+from pathlib import Path
 from random import choice
 
+
+filepath = Path(__file__).parent / "countries_and_capitals.txt"
 
 class WordGenerator:
     def __init__(self):
         self.words = {}
         try:
-            with open("countries_and_capitals.txt", encoding = "utf-8") as f:
+            with open(filepath, encoding = "utf-8") as f:
                 for line in f:
                     (country, capital) = line.split("/")
-                    words[country.strip()] = capital.strip()
+                    self.words[country.strip()] = capital.strip()
         except OSError:
             print("Oops! Could not open/read file containing countries and capitals.")
             print("Please make sure you have countries_and_capitals.txt in your folder.")
